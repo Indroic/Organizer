7fb071455897ae19e2ea4c45367279253eccf046
Indroic <davidlatosefkindriago@gmail.com>
2023-07-23 21:06:12 -0400

    cambios de pruebas

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from file_handler import FileCollector, ExtensionHandler
 from config import Config
 
 
-extensions = ExtensionHandler(extensions=["mp4", "avi"], name="Video", dir_output="C:\\Users\\David Latosefki\\Videos\\probando")
-extensions.save()
-files = FileCollector("C:\\Users\\David Latosefki\\Downloads", extensions)
-files.move()
+extensiones = ExtensionHandler(name="Imagenes")
+extensiones.load()
+
+for i in extensiones.extensions:
+    print(i)
+
+
